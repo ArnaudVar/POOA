061792bf9991f0b301c0ec328b5c6d35f39addee
VictorLeFourn2 <victor.le-fourn@student.ecp.fr>
2019-09-24 19:47:46 +0200

    Doc user + exception pour next_episode

--- classes/user.py
+++ classes/user.py
@@ -57,31 +57,78 @@ class User :
         self.__password = password
 
     def _set_name(self, name):
+
+        """
+        Méthode appelée lorsque l'on veut modifier le prénom de l'utilisateur
+
+        :return: None (on ne peut pas changer le nom de l'utilisateur)
+        """
         return SetterException("On ne peut pas changer le nom de l'utilisateur")
 
     def _set_surname(self, surname):
+
+        """
+        Méthode appelée lorsque l'on veut modifier le nom de l'utilisateur
+
+        :return: None (on ne peut pas changer le nom de l'utilisateur)
+        """
+
         return SetterException("On ne peut pas changer le nom de l'utilisateur")
 
     def _set_id(self, id):
+
+
+        """
+        Méthode appelée lorsque l'on veut modifier l'ID de l'utilisateur
+
+        :return: None (on ne peut pas changer l'ID de l'utilisateur)
+        """
         return SetterException("On ne peut pas changer l'ID de l'utilisateur")
+    id = property(_get_id,_set_id)
+    name = property(_get_name, _set_name)
+    surname = property(_get_surname,_set_surname)
+    password = property(_get_password, _set_password)
+
 
     def add_serie(self, serie):
+
+        """
+        Méthode appelée lorsque l'on veut ajouter une série au dictionnaire des séries que regarde l'utilisateur
+
+        :return: None (type void)
+        """
         self.series[serie.getId()] = 'S1E1'
 
     def remove_serie(self, serie):
+
+        """
+        Méthode appelée lorsque l'on veut enlever une série au dictionnaire des séries que regarde l'utilisateur
+        lorsqu'il ne veut plus la regarder
+        :return: None (type void)
+        """
         del self.series[serie.getId()]
 
     def next_episode(self,serie):
+
+        """
+        Méthode appelée pour une série lorsque l'utilisateur a regardé un épisode et qu'il veut passer au suivant
+
+        :return: None (type void)
+        """
         if serie.getId() not in self.series.keys() :
             raise ValueError("Série non commencée")
         else :
-            separation = self.series[serie.getId()].split('E')
-            number_season = int(separation[0].split('S')[1])
-            number_episode = separation[1]
-            number_episode= int(number_episode)
-            if number_episode == len(serie.seasons[number_season-1].episodes) :
-                self.series[serie.getId()] = 'S'+ str(number_season + 1) +'E1'
+
+            if self.series[serie.id] == serie.latest :
+                print("Vous êtes rendus au dernier épisode en date")
             else :
-                self.series[serie.getId()] = separation[0] + 'E' + str(number_episode + 1)
+                separation = self.series[serie.getId()].split('E')
+                number_season = int(separation[0].split('S')[1])
+                number_episode = separation[1]
+                number_episode= int(number_episode)
+                if number_episode == len(serie.seasons[number_season-1].episodes) :
+                    self.series[serie.getId()] = 'S'+ str(number_season + 1) +'E1'
+                else :
+                    self.series[serie.getId()] = separation[0] + 'E' + str(number_episode + 1)
 
 
