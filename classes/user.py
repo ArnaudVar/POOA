47eab806a0c41c3dad15b281b02386b834bcbd3a
VictorLeFourn2 <victor.le-fourn@student.ecp.fr>
2019-09-24 15:41:39 +0200

    Getter/Setter pour user

--- classes/user.py
+++ classes/user.py
@@ -1,29 +1,50 @@
+from serie import Serie
+from Exception import SetterException
+
 class User :
     __id = 0
     def __init__(self, name, surname, password):
         self.__id = User.__id
         self.__password = password
         self.series = {}
-        self.name = name
-        self.surname = surname
+        self.__name = name
+        self.__surname = surname
+
+    def getName(self):
+        return self.name
+
+    def getSurname(self):
+        return self.surname
+
+    def setName(self):
+        return SetterException()
+
+    def setSurname(self):
+        return SetterException()
+
+    def getId(self):
+        return self.__id
+
+    def setId(self,id):
+        self.__id = id
 
     def add_serie(self, serie):
-        self.series[serie.nom] = 'S1E1'
+        self.series[serie.getId()] = 'S1E1'
 
     def remove_serie(self, serie):
-        del self.series[serie.nom]
+        del self.series[serie.getId()]
 
     def next_episode(self,serie):
-        if serie.nom not in self.series.keys() :
+        if serie.getId() not in self.series.keys() :
             raise ValueError("Série non commencée")
         else :
-            separation = self.series[serie.nom].split('E')
+            separation = self.series[serie.getId()].split('E')
             number_season = int(separation[0].split('S')[1])
             number_episode = separation[1]
             number_episode= int(number_episode)
             if number_episode == len(serie.seasons[number_season-1].episodes) :
-                self.series[serie.nom] = 'S'+ str(number_season + 1) +'E1'
+                self.series[serie.getId()] = 'S'+ str(number_season + 1) +'E1'
             else :
-                self.series[serie.nom] = separation[0] + 'E' + str(number_episode + 1)
+                self.series[serie.getId()] = separation[0] + 'E' + str(number_episode + 1)
 
 
