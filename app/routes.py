0e63a337be3a36a89ca63ae3473c20ca17b51211
Arnaud Vargas <arnaud.vargas@wanadoo.fr>
2019-10-03 14:25:31 +0200

    Forms et routes

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,6 @@
-from flask import render_template, redirect, url_for
 from app import app
-from app.forms import LoginForm
-
 
 @app.route('/')
-def home():
-    return render_template('home.html', title='Home')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect(url_for('home'))
-    return render_template('login.html', title='Sign In', form=form)
+@app.route('/index')
+def index():
+    return "Hello, World!"
