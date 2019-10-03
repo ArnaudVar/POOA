a49c3ea8d81740ba398814bcf18f8814890251f2
Arnaud Vargas <arnaud.vargas@wanadoo.fr>
2019-10-03 14:23:47 +0200

    init route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
+from flask import render_template, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+def home():
+    return render_template('home.html', title='Home')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Sign In', form=form)
