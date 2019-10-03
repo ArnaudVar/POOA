d3d233d8e046faa66b639fab2f1d5fdcef15df71
Hippolyte <hippolyte.favreau@student.ecp.fr>
2019-10-03 15:30:00 +0200

    First implementation of Login

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,46 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
+from flask import request
+from flask_login import login_required
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
+@app.routes('/home')
+@login_required
 def home():
     return render_template('home.html', title='Home')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Sign In')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(user_name=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember = form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
 
 @app.route('/serie')
+@login_required
 def serie():
     return render_template('serie.html', title='Serie')
