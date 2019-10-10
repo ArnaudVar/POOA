adb64f39fae8472c49e2e9352d444e4fc9d7e87d
Hippolyte <hippolyte.favreau@student.ecp.fr>
2019-10-10 21:43:41 +0200

    SearchPage

--- app/routes.py
+++ app/routes.py
@@ -90,6 +90,9 @@ def remove_serie(id):
     return('',204)
 
 
+
+
+
 @app.route('/myseries/<user_id>')
 @login_required
 def myserie(user_id):
@@ -105,5 +108,18 @@ def myserie(user_id):
         for tvshow in list_series:
             nb_series+=1
             r = requests.get(f"{base_url_start}{tvshow[0]}{base_url_end}").json()
-            list_serie_rendered.append(Serie(r['id'],r['name'], r['overview'], r['vote_average'], r['genres'], r['poster_path'], {}, len(r['seasons']), r['last_episode_to_air'], r['next_episode_to_air']['air_date']))
+            list_serie_rendered.append(Serie(r['id'],r['name'], r['overview'], r['vote_average'], r['genres'], r['poster_path'], {}, len(r['seasons']), r['last_episode_to_air'], ''))
     return render_template('mySeries.html', title='MySeries', list_series=list_serie_rendered, nb_series=nb_series)
+
+@app.route('/search/<string>/<page>')
+@login_required
+def search(string, page):
+    base_url_tv = f"https://api.themoviedb.org/3/search/tv?query={string}&api_key={api_key}&language=en-US&page={page}"
+    base_url_movies = f"https://api.themoviedb.org/3/search/movie?query={string}&api_key={api_key}&language=en-US&page={page}"
+    r1 = requests.get(base_url_tv).json()
+    r2 = requests.get(base_url_movies).json()
+    list_series = r1['results']
+    print(list_series)
+    list_movies = r2['results']
+    nb_pages = max(r1['total_pages'], r2['total_pages'])
+    return render_template('search.html', title='Search', list_series=list_series, list_movies=list_movies, nb_pages=nb_pages, search = string)
