ff97c45d768e589b89ee2ce68b6522a9bafc6965
Arnaud Vargas <arnaud.vargas@wanadoo.fr>
2019-10-11 09:05:43 +0200

    email support to reset password

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
-from app import db
-from werkzeug.security import  generate_password_hash, check_password_hash
+from app import db, login, app
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import login
+import jwt
+from time import time
+
 
 
 class User(UserMixin, db.Model):
@@ -47,6 +49,20 @@ class User(UserMixin, db.Model):
             self._series = f"{id_serie}xS1E1"
         else:
             self._series += f"-{id_serie}xS1E1"
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
 
 
 @login.user_loader
