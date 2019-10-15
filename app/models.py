4b38a21179f27ed784b2d4b7012ec4498261e812
VictorLeFourn2 <victor.le-fourn@student.ecp.fr>
2019-10-15 15:04:33 +0200

    "Next Episode" Dr Dre feat Snoop Dogg

--- app/models.py
+++ app/models.py
@@ -49,7 +49,7 @@ class User(UserMixin, db.Model):
         db.session.commit()
         return(self.series is not None and str(id) in self.series)
 
-    def is_after(self, episode, serie):
+    def is_after(self, season, episode, serie):
         if str(serie) not in self.series:
             return True
         else :
@@ -58,7 +58,7 @@ class User(UserMixin, db.Model):
                 if int(serie_string.split('x')[0]) == serie :
                     code = serie_string.split('x')[1]
                     code_last = code.split('E')
-            return(int(code_last[0].split('S')[1]) > episode.num_season or (int(code_last[0].split('S')[1]) == episode.num_season and int(code_last[1]) < episode.num_episode) )
+            return(int(code_last[0].split('S')[1]) > season or (int(code_last[0].split('S')[1]) == season and int(code_last[1]) < episode) )
 
 
 
@@ -68,7 +68,6 @@ class User(UserMixin, db.Model):
             if userserie.split('x')[0] == str(serie) :
                 last_episode_watched = userserie.split('x')[1]
                 new_series = self.series.replace(userserie, userserie.replace(last_episode_watched, episode))
-                print('new',new_series)
                 self._set_series(new_series)
                 db.session.commit()
 
